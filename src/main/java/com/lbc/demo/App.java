b2773ee1fa70a99dca5218915e140b08b174661c
LBC <316678357@qq.com>
2019-03-07 22:38:21 +0800

    First Commit.

---
+++ src/main/java/com/lbc/demo/App.java
@@ -0,0 +1,13 @@
+package com.lbc.demo;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println();
+        System.out.println();
+
+    }
+}
